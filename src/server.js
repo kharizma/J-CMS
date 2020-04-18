162873b06f6286f07af8a0050ef81ee67c8670da
Ully Kharisma Putra <ullykharisma@gmail.com>
2020-04-18 12:25:52 +0700

    setting template engine handlebars

--- src/server.js
+++ src/server.js
@@ -1,12 +1,21 @@
 const express = require('express')
-const hbs = require('handlebars')
+const exphbs = require('express-handlebars')
+const path = require('path')
 
 const app = express();
 const port = 3000;
 
-const template = hbs.compile("Name: {{name}}");
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
 
-app.get('/', (req,res) => res.send(template({ name: "Nils" })));
+app.set('view engine', 'handlebars');
+
+app.set('views', path.join(__dirname,'views'));
+
+app.get('/', (req,res) => {
+  res.render('home');
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
